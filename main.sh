9da2d2b4f4b1e1b04ae8c5202aae37205b3bf8ef
tama3ti <m.picheta@icloud.com>
2022-11-12 23:13:40 +0100

    add ansible script of docker and git clone project

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source openstack/openstack-vars-openrc.sh
-openstack stack create --template openstack/heat_vm.yaml Humid
+openstack stack create --template openstack/heat_vm.yaml Humidity
+echo "waiting for vm creation..."
+sleep 60
+ansible-playbook -i ansible/hosts ansible/main.yaml
