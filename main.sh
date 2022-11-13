d541e425cff375d969a2ea6bd7793c73e0b14720
tama3ti <m.picheta@icloud.com>
2022-11-13 22:05:17 +0100

    add variable replacement for password insertion to java project

--- main.sh
+++ main.sh
@@ -3,4 +3,14 @@ source openstack/openstack-vars-openrc.sh
 openstack stack create --template openstack/heat_vm.yaml Humidity
 echo "waiting for vm creation..."
 sleep 60
-ansible-playbook -i ansible/hosts ansible/main.yaml
+
+echo "insert keystore passwd: "
+read var_keystore_passwd
+echo "insert db url"
+read var_dburl
+echo "insert db user"
+read var_dbusr
+echo "insert db passwd"
+read var_dbpasswd
+ansible-playbook -i ansible/hosts ansible/main.yaml --extra-vars="KEYSTOREPASSWD=$var_keystore_passwd DBURL=$var_dburl DBUSR=$var_dbusr DBPASSWD=$var_dbpasswd"
+
